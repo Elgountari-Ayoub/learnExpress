e02cc20ea76435c89a2aab84c6a36c8d73a7de2d
PM <elgountariayoub21@gmail.com>
2024-07-05 16:11:16 +0100

    advanced routing

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const router = express.Router()
+router.set(toggler( ))
+
+router.get("/", (req, res) => {
+    res.send("User List")
+})
+
+router.get("/new", (req, res) => {
+    res.send("User New Form")
+})
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+router.route('/:id')
+    .get((req, res) => {
+        res.send('Hello user number ' + req.userNumber)
+    })
+    .post((req, res) => {
+        res.send('Hello user number ' + req.userNumber)
+    }).put((req, res) => {
+        res.send('Hello user number ' + req.userNumber)
+    }).delete((req, res) => {
+        res.send('Hello user number ' + req.userNumber)
+    })
+router.param("id", (req, res, next, id) => {
+    console.log(id);
+    req.userNumber = "01"
+    next()
+})
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    //res.send("Hello Express")
-    //res.sendStatus(500) // the default status is 200
-    // res.download('server.js') // when this method runs, the server.js file will be downloaded
-    //res.status(200).send("We have a problem here....!")
-    
-    // res.json({
-    //     "name": "Ayoub",
-    //     "age": 26
-    // })
-
-    //Render a file [HTML file] called index.html you just passed the index
-    res.render('index', {text : 'Express'})
+app.set("view engine", "ejs")
+app.get("/", (req, res) => {
+    console.log("Here")
+    res.render("index", { text: "World" })
 })
+const userRouter = require("./routes/users")
+app.use("/users", userRouter)
 app.listen(3000)
