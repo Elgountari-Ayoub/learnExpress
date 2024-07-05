b941fb38beaa7693faa6c499ac0340de0639ead6
PM <elgountariayoub21@gmail.com>
2024-07-05 16:31:56 +0100

    rendering  static files

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 
 app.set("view engine", "ejs")
+
+app.use(express.static('public'))
 // app.use(logger)
-app.get("/", logger, (req, res) => {
-    // console.log("Here")
-    res.render("index", { text: "World" })
-})
+// app.get("/", logger, (req, res) => {
+//     // console.log("Here")
+//     res.render("index", { text: "World" })
+// })
 
 const userRouter = require("./routes/users")
 app.use("/users", userRouter)
